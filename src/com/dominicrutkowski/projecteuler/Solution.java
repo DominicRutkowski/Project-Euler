b0f462ea51bbf7a85491aa111c406fc3d0f99a3c
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-03-31 21:07:26 -0500

    Unit tests, interface for solutions

---
+++ src/com/dominicrutkowski/projecteuler/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Project Euler Solutions
+ * Copyright (c) 2018 Dominic Rutkowski
+ *
+ * LICENSE:
+ * https://github.com/DominicRutkowski/Project-Euler/blob/master/LICENSE
+ */
+
+package com.dominicrutkowski.projecteuler;
+
+public interface Solution {
+
+    String run();
+
+}
