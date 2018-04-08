58de536ee5b1fddecc956a57d493d1bec916059d
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-04-08 18:09:03 -0500

    All solutions now measure and print the elapsed time when tested.

--- src/com/dominicrutkowski/projecteuler/Solution.java
+++ src/com/dominicrutkowski/projecteuler/Solution.java
@@ -11,17 +11,26 @@ package com.dominicrutkowski.projecteuler;
 public abstract class Solution {
 
     private String answer;
+    private long time;
 
     public abstract String run();
 
     public String getAnswer() {
         System.out.println("Problem: " + this.getClass().getSimpleName().substring(1));
-        System.out.println("Answer: " + answer + "\n");
+        System.out.println("Answer: " + answer);
+        System.out.println("Time: " + time + " nanoseconds");
+        if (time > 60000000000L) {
+            System.out.println("WARNING: Calculation time exceeds one minute.");
+        }
+        System.out.println();
         return answer;
     }
 
     public Solution() {
+        final long startTime = System.nanoTime();
         answer = run();
+        final long stopTime = System.nanoTime();
+        time = stopTime - startTime;
     }
 
 }
