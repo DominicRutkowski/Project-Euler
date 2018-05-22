c8f55de446f2a905dd35e3e543aa5ffe77f8a79c
DominicRutkowski <rutkowski.dominic@gmail.cojm>
2018-05-22 11:28:49 -0500

    Removed old P0179

--- src/com/dominicrutkowski/projecteuler/solutions/P0179.java
+++ src/com/dominicrutkowski/projecteuler/solutions/P0179.java
@@ -13,7 +13,7 @@ package com.dominicrutkowski.projecteuler.solutions;
 
 import com.dominicrutkowski.projecteuler.Solution;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class P0179 extends Solution {
 
@@ -21,49 +21,11 @@ public class P0179 extends Solution {
 
     @Override
     public String run() {
-        int satisfyCondition = 0;
-        long firstNumberDivisors;
-        long secondNumberDivisors = 2;
-        for (int i = 2; i < MAX; i++) {
-            firstNumberDivisors = secondNumberDivisors;
-            secondNumberDivisors = noOfDivisors(i + 1);
-            if (firstNumberDivisors == secondNumberDivisors) {
-                satisfyCondition++;
-            }
+        int[] divisors = new int[MAX + 1];
+        Arrays.fill(divisors, 2);
+        for (int i = 2; i < MAX + 1; i++) {
+            
         }
-        return Integer.toString(satisfyCondition);
-    }
-
-    private long noOfDivisors(int n) {
-//        long count = 0;
-//        for (int i = 1; Math.pow(i, 2) <= n; i++) {
-//            if (n % i == 0) {
-//                count += 2;
-//            }
-//        }
-//        if (Math.pow((int) Math.sqrt(n), 2) == n) {
-//            count--;
-//        }
-//        return count;
-        HashMap<Integer, Integer> primeFactorization = new HashMap<>();
-        while (n != 1) {
-            for (int i = 2;; i++) {
-                if (n % i == 0) {
-                    n %= i;
-                    if (primeFactorization.containsKey(i)) {
-                        primeFactorization.put(i, primeFactorization.get(i + 1));
-                    } else {
-                        primeFactorization.put(i, 1);
-                    }
-                    break;
-                }
-            }
-        }
-        int factors = 1;
-        for (int exponent : primeFactorization.values()) {
-            factors *= exponent + 1;
-        }
-        return factors;
     }
 
 }
